0806dd65740a4ec4222aaf87da1526417a21196a
Label54 <yasarkof54@gmail.com>
2025-06-28 16:46:10 +0300

    Delete ipkg-build.sh

--- ipkg-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mkdir -p rtl8812bu_package/lib/modules/4.4.35
-cp 88x2bu.ko rtl8812bu_package/lib/modules/4.4.35/
-echo "Package: rtl8812bu
-Version: 1.0
-Architecture: armv7
-Description: RTL8812BU WiFi Driver for Enigma2" > rtl8812bu_package/control
-
-ipkg-build rtl8812bu_package .
